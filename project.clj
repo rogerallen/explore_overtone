5b7c7a19895e88b285a050ae939801659e3fd3a6
Roger Allen <rallen@gmail.com>
2012-04-15 21:09:23 -0700

    adding basics for overtone + emacs.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject explore_overtone "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject explore_overtone "0.0.0"
+  :description "Explorations while trying to learn overtone."
+  :dependencies [ [org.clojure/clojure "1.3.0"]
+                  [overtone "0.6.0"] ]
+  :plugins [ [lein-swank "1.4.4"] ]
+  )
