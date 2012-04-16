3f4707f80461a9082ba187d482b94039b58b0ad1
Roger Allen <rallen@gmail.com>
2012-04-15 20:32:58 -0700

    initial commit.  no changes for overtone yet.

---
+++ src/explore_overtone/core.clj
@@ -0,0 +1,1 @@
+(ns explore_overtone.core)
---
+++ test/explore_overtone/test/core.clj
@@ -0,0 +1,6 @@
+(ns explore_overtone.test.core
+  (:use [explore_overtone.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
