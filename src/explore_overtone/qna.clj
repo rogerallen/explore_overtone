986dae74f014230bab19bb5b2c7a909326c82336
Roger Allen <rallen@gmail.com>
2012-04-16 22:16:24 -0700

    latest q&a updates

--- src/explore_overtone/qna.clj
+++ src/explore_overtone/qna.clj
@@ -3,19 +3,16 @@
 
 ;; ......................................................................
 ;; * how do I start overtone?
-
 (use 'overtone.live)
 
 ;; ......................................................................
 ;; * how do I start the external server
 ;; ......................................................................
 ;; * how do I find documentation on 'demo' or anything else
-
 (odoc demo)
 
 ;; ......................................................................
 ;; * how do we stop repl in a good way?
-
 (System/exit 0)
 
 ;; this might be handy
@@ -26,20 +23,23 @@
 ;; * how do I load/use other files containing code? 
 ;; ......................................................................
 ;; * how to play a simple sine wave?
-
-;; 3 seconds of a 300 Hz sine wave
-(demo 3 (sin-osc 300))
+(demo 3 (sin-osc 300)) ; 3 seconds of a 300 Hz sine wave
 
 ;; ......................................................................
 ;; * how to connect to OSC ipad or android controllers
 
 ;; get http://hexler.net/software/touchosc for ipad/android + bridge
-;; bridge app doesn't show any gui.
+;; bridge app doesn't show any gui, but there is something in the menu bar.
+;;
+;; control also seems to work
+;; http://itunes.apple.com/us/app/control-osc-+-midi/id413224747?mt=8
+;; http://charlie-roberts.com/Control/
 
 (midi-in) ; brings up a dialog, but it can 'pop under' so look for it
 
 ;; just listen to the events coming in & 
 (def kb (midi-in "TouchOSC Bridge"))
+(def kb (midi-in "Control Session"))
 (defn midi-listener [event ts]
   (println "listener: " event))
 (midi-handle-events kb #'midi-listener)
@@ -74,11 +74,41 @@
 ;; * how do I play quality sampled instruments
 ;; ......................................................................
 ;; * how do I save the audio?
+I saw this on the main website...
 ;; ......................................................................
 ;; * how do I add "presence"/"echo"/"reverb" effects to a song.
 ;; ......................................................................
 ;; * what is the history/difference between defsynth & definst?
+;; ......................................................................
+;; * how would I do this?  http://korg.com/monotrons# see schematic at
+;;   http://korg.com/services/products/monotron/monotron_Block_diagram.jpg
+;; ......................................................................
+;; * What is wrong with this?
 
+;; this works
+(demo 3 (saw 300))
 
+;; ??? this causes exception
+(definst foo
+  "foo"
+  [freq 440]
+  (saw freq))
+(demo 3 (foo 300))
+;; Evaluation aborted.
+;; Error in checker for ugen out. out:ar requires that all incoming signals be audio rate
+;; ???
 
+;; but this seems to work
+(foo 300)
+(stop)
+;; or 
+(kill foo)
 
+;; ......................................................................
+;; * why can't I do a function around inst?
+(defn lerp
+  "linear interpoloate between a & b.  alpha=0 for a, alpha=1 for b."  
+  [alpha a b]
+  (+ (* (- 1 alpha) a)
+     (* alpha b)))
+(lerp 0.5 (sin-osc 440) (sin-osc 330))
