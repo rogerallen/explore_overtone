d49ee60c1956ade83141f59cdc2bdf86941fb02c
Roger Allen <rallen@gmail.com>
2012-05-13 08:16:53 -0700

    refactor the code for clarity

--- src/explore_overtone/irrational_infinite_song.clj
+++ src/explore_overtone/irrational_infinite_song.clj
@@ -35,65 +35,75 @@
   9 5 7 7 8 1 8 5 7 7 8 0 5 3 2 1 7 1 2 2 6 8 0 6 6 1 3 0 0 1 9 2 7 8
   7 6 6 1 1 1 9 5 9 0 9 2 1 6 4 2 0 1 9 8 9))
 
-;; use this to get a tuple (note velocity duration) of indexes
-(defn s3v [s]
-  "given a list s, make it into a list of 3-vectors"
-  (map vector
-       (doall (take-nth 3 s))
-       (doall (take-nth 3 (nthrest s 1)))
-       (doall (take-nth 3 (nthrest s 2)))))
+;; A note on terminology...in this code a 'note' has pitch, duration &
+;; velocity.  Overtone generally refers to this pitch as the note, but
+;; the code here makes a distinction.
+;;
+;; inote = a note of indices, before they are translated to the proper
+;; scale & otherwise made ready to play
+;;
+;; snote = a sequence note after translation & ready to play with an
+;; associated beat to tell when it will be played.
 
-(defn index2note [tonic type n]
-  "given a digit 'n' in range 0..9 find index in scale defined by
-     tonic & type.  E.g. (index2note :c4 :major 1) -> 62"
-  (nth (vec (scale tonic type (range 1 10))) (mod n 10)))
+(defn digits2inotes [digit-seq]
+  "given a list of digits, make it into a list of index notes"
+  (map #(hash-map :pitch-index %1 :velocity-index %2 :duration-index %3)
+       (take-nth 3 digit-seq)
+       (take-nth 3 (nthrest digit-seq 1))
+       (take-nth 3 (nthrest digit-seq 2))))
 
-(defn index2velocity [n]
+(defn index2pitch [tonic type index]
+  "given a digit in range 0..9 find index in scale defined by
+     tonic & type.  E.g. (index2pitch :c4 :major 1) -> 62"
+  (nth (vec (scale tonic type (range 1 10))) (mod index 10)))
+
+(defn index2velocity [index]
   "given a digit 'n' in range 0..9, find a velocity to play"
-  (+ 80 (* 3 n)))
+  (+ 80 (* 3 index)))
 
-(defn index2duration [n]
+(defn index2duration [index]
   "given a digit 'n' in range 0..9, find a length in beats"
   ;; 1/f histogram of length
   ;;  0    1    2    3    4    5    6    7    8    9
-  ([ 4.00 2.00 1.33 1.00 0.80 0.66 0.57 0.50 0.44 0.40] n))
+  ([ 4.00 2.00 1.33 1.00 0.80 0.66 0.57 0.50 0.44 0.40] index))
 
-(defn iii2nvd0 [tonic type cur-3v]
-  "given (n v d) indexes, fix (n v d 0) ready to play"
-  (let [ret-3v-n (index2note tonic type (nth cur-3v 0))
-        ret-3v-v (index2velocity (nth cur-3v 1))
-        ret-3v-d (index2duration (nth cur-3v 2))]
-    (list ret-3v-n ret-3v-v ret-3v-d 0)))
+(defn inote2snote [tonic type cur-inote]
+  "given an index-note, create a sequence-note with a place for a beat."
+  (hash-map
+   :pitch (index2pitch tonic type (:pitch-index cur-inote))
+   :velocity (index2velocity (:velocity-index cur-inote))
+   :duration (index2duration (:duration-index cur-inote))
+   :beat 0))
 
-(defn duration2beat [cur-4v nxt-4v]
-  "given 2 (n v d 0) tuples, sum the duration to give a beat for the nxt one"
-  (let [ret-4v-n (nth nxt-4v 0)
-        ret-4v-v (nth nxt-4v 1)
-        ret-4v-d (nth nxt-4v 2)
-        ret-4v-b (+ (nth cur-4v 2) (nth cur-4v 3))]
-    (list ret-4v-n ret-4v-v ret-4v-d ret-4v-b)))
+(defn duration2beat [cur-snote nxt-snote]
+  "given 2 sequence notes, update the nxt beat"
+  (hash-map
+   :pitch (:pitch nxt-snote)
+   :velocity (:velocity nxt-snote)
+   :duration (:duration nxt-snote)
+   :beat (+ (:duration cur-snote) (:beat cur-snote))))
 
-(defn num-beats [seq]
-  "how long is a sequence? last duration + last beat"
-  (let [last-4v (nth seq (dec (count seq)))]
-    (+ (nth last-4v 2) (nth last-4v 3))))
+(defn num-beats [snote-seq]
+  "how long is a snote sequence? last duration + last beat"
+  (let [last-snote (last snote-seq)]
+    (+ (:beat last-snote) (:duration last-snote))))
 
 (defn calc-seq [tonic type num-notes offset the-series]
-  "calc some notes in a certain key. doall to remove laziness. returns a list of
-   (note velocity duration curbeat) values"
+  "calc some seq-notes in a certain key. doall to remove laziness. returns a list of
+   (pitch velocity duration curbeat) values"
   (doall (reductions duration2beat
-                     (map #(iii2nvd0 tonic type %)
-                          (take num-notes (nthrest (s3v the-series) offset))))))
+                     (map #(inote2snote tonic type %)
+                          (take num-notes (nthrest (digits2inotes the-series) offset))))))
 
-(defn play-seq [m beat seq]
-  "play a list of (note velocity duration curbeat) where seq is offset by beat"
-  (doseq [cur-4v seq]
-    (let [cur-note (nth cur-4v 0)
-          cur-vel (nth cur-4v 1)
-          cur-dur (nth cur-4v 2)
-          cur-beat (+ beat (nth cur-4v 3))]
-      ;;(println "play:" (+ beat cur-beat) cur-note cur-vel)
-      (at (m cur-beat) (piano cur-note 1 cur-vel)))))
+(defn play-seq [m beat snote-seq]
+  "play a list of (pitch velocity duration curbeat) where snote-seq is offset by beat"
+  (doseq [cur-snote snote-seq]
+    (let [cur-pitch (:pitch cur-snote)
+          cur-vel (:velocity cur-snote)
+          cur-dur (:duration cur-snote)
+          cur-beat (+ beat (:beat cur-snote))]
+      ;;(println "play:" (+ beat cur-beat) cur-pitch cur-vel)
+      (at (m cur-beat) (piano cur-pitch 1 cur-vel)))))
 
 (defn infinite-song [m beat tonic type]
   (println "infinite song" beat tonic type)
@@ -109,6 +119,6 @@
   (apply-at (m seq2-start) #'infinite-song m seq2-start tonic type []))
 
 ;; and now, we play...
-(def metro (metronome 120))
-(infinite-song metro (metro) :e3 :pentatonic)
+(def metro (metronome 60))
+(infinite-song metro (metro) :d3 :major)
 ;;(stop) when you must.
