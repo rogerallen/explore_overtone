db82a79ac11b96678657cd809c0eef3f87064bb5
Roger Allen <rallen@gmail.com>
2012-05-13 15:26:33 -0700

    figured out how to build and use local 0.7.0 build

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject explore_overtone "0.0.0"
   :description "Explorations while trying to learn overtone."
+  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [ [org.clojure/clojure "1.3.0"]
-                  [overtone "0.6.0"] ]
+                  ;;[overtone "0.6.0"]
+                  [overtone "0.7.0-LOCALSNAPSHOT" ]
+                  ]
   :plugins [ [lein-swank "1.4.4"] ]
   )
