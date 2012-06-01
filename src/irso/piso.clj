16837835de94385fd32ec864d7aeac326a7b73d8
Roger Allen <rallen@gmail.com>
2012-05-31 23:46:10 -0700

    initial sounds

---
+++ src/irso/piso.clj
@@ -0,0 +1,195 @@
+(ns irso.piso)
+(use 'overtone.core)
+(connect-external-server 57110)
+(use 'overtone.inst.sampled-piano) ;; requires 0.7.0. downloads 200MB
+
+;; pi to 1000 digits
+(def pi1000 '(3 1 4 1 5 9 2 6 5 3 5 8 9 7 9 3 2 3 8 4 6 2 6 4 3 3 8 3
+  2 7 9 5 0 2 8 8 4 1 9 7 1 6 9 3 9 9 3 7 5 1 0 5 8 2 0 9 7 4 9 4 4 5
+  9 2 3 0 7 8 1 6 4 0 6 2 8 6 2 0 8 9 9 8 6 2 8 0 3 4 8 2 5 3 4 2 1 1
+  7 0 6 7 9 8 2 1 4 8 0 8 6 5 1 3 2 8 2 3 0 6 6 4 7 0 9 3 8 4 4 6 0 9
+  5 5 0 5 8 2 2 3 1 7 2 5 3 5 9 4 0 8 1 2 8 4 8 1 1 1 7 4 5 0 2 8 4 1
+  0 2 7 0 1 9 3 8 5 2 1 1 0 5 5 5 9 6 4 4 6 2 2 9 4 8 9 5 4 9 3 0 3 8
+  1 9 6 4 4 2 8 8 1 0 9 7 5 6 6 5 9 3 3 4 4 6 1 2 8 4 7 5 6 4 8 2 3 3
+  7 8 6 7 8 3 1 6 5 2 7 1 2 0 1 9 0 9 1 4 5 6 4 8 5 6 6 9 2 3 4 6 0 3
+  4 8 6 1 0 4 5 4 3 2 6 6 4 8 2 1 3 3 9 3 6 0 7 2 6 0 2 4 9 1 4 1 2 7
+  3 7 2 4 5 8 7 0 0 6 6 0 6 3 1 5 5 8 8 1 7 4 8 8 1 5 2 0 9 2 0 9 6 2
+  8 2 9 2 5 4 0 9 1 7 1 5 3 6 4 3 6 7 8 9 2 5 9 0 3 6 0 0 1 1 3 3 0 5
+  3 0 5 4 8 8 2 0 4 6 6 5 2 1 3 8 4 1 4 6 9 5 1 9 4 1 5 1 1 6 0 9 4 3
+  3 0 5 7 2 7 0 3 6 5 7 5 9 5 9 1 9 5 3 0 9 2 1 8 6 1 1 7 3 8 1 9 3 2
+  6 1 1 7 9 3 1 0 5 1 1 8 5 4 8 0 7 4 4 6 2 3 7 9 9 6 2 7 4 9 5 6 7 3
+  5 1 8 8 5 7 5 2 7 2 4 8 9 1 2 2 7 9 3 8 1 8 3 0 1 1 9 4 9 1 2 9 8 3
+  3 6 7 3 3 6 2 4 4 0 6 5 6 6 4 3 0 8 6 0 2 1 3 9 4 9 4 6 3 9 5 2 2 4
+  7 3 7 1 9 0 7 0 2 1 7 9 8 6 0 9 4 3 7 0 2 7 7 0 5 3 9 2 1 7 1 7 6 2
+  9 3 1 7 6 7 5 2 3 8 4 6 7 4 8 1 8 4 6 7 6 6 9 4 0 5 1 3 2 0 0 0 5 6
+  8 1 2 7 1 4 5 2 6 3 5 6 0 8 2 7 7 8 5 7 7 1 3 4 2 7 5 7 7 8 9 6 0 9
+  1 7 3 6 3 7 1 7 8 7 2 1 4 6 8 4 4 0 9 0 1 2 2 4 9 5 3 4 3 0 1 4 6 5
+  4 9 5 8 5 3 7 1 0 5 0 7 9 2 2 7 9 6 8 9 2 5 8 9 2 3 5 4 2 0 1 9 9 5
+  6 1 1 2 1 2 9 0 2 1 9 6 0 8 6 4 0 3 4 4 1 8 1 5 9 8 1 3 6 2 9 7 7 4
+  7 7 1 3 0 9 9 6 0 5 1 8 7 0 7 2 1 1 3 4 9 9 9 9 9 9 8 3 7 2 9 7 8 0
+  4 9 9 5 1 0 5 9 7 3 1 7 3 2 8 1 6 0 9 6 3 1 8 5 9 5 0 2 4 4 5 9 4 5
+  5 3 4 6 9 0 8 3 0 2 6 4 2 5 2 2 3 0 8 2 5 3 3 4 4 6 8 5 0 3 5 2 6 1
+  9 3 1 1 8 8 1 7 1 0 1 0 0 0 3 1 3 7 8 3 8 7 5 2 8 8 6 5 8 7 5 3 3 2
+  0 8 3 8 1 4 2 0 6 1 7 1 7 7 6 6 9 1 4 7 3 0 3 5 9 8 2 5 3 4 9 0 4 2
+  8 7 5 5 4 6 8 7 3 1 1 5 9 5 6 2 8 6 3 8 8 2 3 5 3 7 8 7 5 9 3 7 5 1
+  9 5 7 7 8 1 8 5 7 7 8 0 5 3 2 1 7 1 2 2 6 8 0 6 6 1 3 0 0 1 9 2 7 8
+  7 6 6 1 1 1 9 5 9 0 9 2 1 6 4 2 0 1 9 8 9))
+
+;; change to play the first digits as pitches
+(defn digits2inotes [digit-seq]
+  "given a list of digits, make it into a list of index notes"
+  (let [n (int (/ (count digit-seq) 3))]
+    (map #(hash-map :pitch-index %1 :velocity-index %2 :duration-index %3)
+         (take n digit-seq)
+         (take n (drop n digit-seq))
+         (take n (drop (* 2 n) digit-seq)))))
+
+(defn index2pitch [tonic type index]
+  "given a digit in range 0..9 find index in scale defined by
+     tonic & type.  E.g. (index2pitch :c4 :major 1) -> 62"
+  (nth (vec (scale tonic type (range 1 10))) (mod index 10)))
+
+(defn index2velocity [index]
+  "given a digit 'n' in range 0..9, find a velocity to play"
+  (+ 80 (* 3 index)))
+
+(defn index2duration [index]
+  "given a digit 'n' in range 0..9, find a length in beats"
+  (cond ;; pick one below...
+    ;;        0    1    2    3    4    5    6    7    8    9
+    false ([ 4.00 2.00 1.33 1.00 0.80 0.66 0.57 0.50 0.44 0.40] index)  ;; 1/f
+    false  ([ 4.00 2.00 1.50 1.00 0.75 0.75 0.50 0.50 0.50 0.25] index)  ;; 16x range
+    true ([ 4.00 2.00 1.50 1.00 0.75 0.75 0.50 0.50 0.50 0.50] index)  ;; 8x range
+    false ([ 4.00 2.00 1.50 1.00 0.75 0.75 0.50 0.50 0.50 0.25] index))) ;; 4x range
+
+(defn inote2snote [tonic type cur-inote]
+  "given an index-note, create a sequence-note with a place for a beat."
+  (hash-map
+   :pitch (index2pitch tonic type (:pitch-index cur-inote))
+   :velocity (index2velocity (:velocity-index cur-inote))
+   :duration (index2duration (:duration-index cur-inote))
+   :beat 0))
+
+(defn duration2beat [cur-snote nxt-snote]
+  "given 2 sequence notes, update the nxt beat"
+  (hash-map
+   :pitch (:pitch nxt-snote)
+   :velocity (:velocity nxt-snote)
+   :duration (:duration nxt-snote)
+   :beat (+ (:duration cur-snote) (:beat cur-snote))))
+
+(defn linear-map [x0 x1 y0 y1 x]
+  "given x0 -> y0.  x1 -> y1.  x maps linearly to y"
+  (let [dydx (/ (- y1 y0) (- x1 x0))
+        dx (- x x0)]
+    (+ y0 (* dydx dx))))
+        
+(defn velocity2attack [v]
+  "sampled-piano uses attack & level, not velocity"
+  (linear-map 0 127 0.2 0.05 v))
+
+(defn velocity2level [v]
+  "sampled-piano uses attack & level, not velocity"
+  (linear-map 0 127 0.0 0.8 v))
+
+(defn num-beats [snote-seq]
+  "how long is a snote sequence? last duration + last beat"
+  (let [last-snote (last snote-seq)]
+    (+ (:beat last-snote) (:duration last-snote))))
+
+(defn calc-seq [tonic type num-notes offset the-series]
+  "calc some seq-notes in a certain key. doall to remove laziness. returns a list of
+   (pitch velocity duration curbeat) values"
+  (doall (reductions duration2beat
+                     (map #(inote2snote tonic type %)
+                          (take num-notes (nthrest (digits2inotes the-series) offset))))))
+
+(defn play-seq [m beat snote-seq]
+  "play a list of (pitch velocity duration curbeat) where snote-seq is offset by beat"
+  (doseq [cur-snote snote-seq]
+    (let [cur-pitch (:pitch cur-snote)
+          cur-attack (velocity2attack (:velocity cur-snote))
+          cur-level (velocity2level (:velocity cur-snote))
+          cur-dur (:duration cur-snote)
+          cur-beat (+ beat (:beat cur-snote))
+          k-beat 1.6]
+      ;;(println "note-on:" beat cur-beat cur-pitch cur-snote)
+      (at (m cur-beat) (def pk (sampled-piano :note cur-pitch
+                                              :level cur-level
+                                              :attack cur-attack)))
+      ;;(println "note-off:" (+ cur-beat (* k-beat cur-dur)))
+      (at (m (+ cur-beat (* k-beat cur-dur))) (ctl pk :gate 0)))))
+
+;; ======================================================================
+;; the song
+;;seq2 (calc-seq tonic type 17 13 pi1000)
+;;(def seq2-start (+ (m) 17))
+;;(def nxt-start (+ (m) seq-len 17))
+
+;; (defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
+
+(defn piso0 [m beat tonic type]
+  (let [seq1 (calc-seq tonic type 13 0 pi1000)]
+    (println "play-seq0" beat seq1)
+    (play-seq m beat seq1)))
+
+(defn piso1 [m beat tonic type]
+  (let [seq1 (calc-seq tonic type 13 0 pi1000)
+        seq1-len (num-beats seq1)
+        num-play-rests 8
+        pi-seq (take num-play-rests pi1000)
+        repeat-counts (take-nth 2 pi-seq)
+        rest-count-sums (conj (take-nth 2 (drop 1 (reductions + pi-seq))) 0)
+        seq-indexes (doall (flatten (map #(map (fn [x] (+ %2 x)) %1)
+                                         (map range repeat-counts)
+                                         rest-count-sums)))
+        ]
+    ;;(println seq-indexes)
+    ;; 0,1,2,[3],4,5,6,7,[8],9,10,11,12,13,[14...17],18
+    (println "play-seq1 indexes & len" seq-indexes seq1-len)
+    (doseq [cur-index seq-indexes]
+      (println "play-seq1" cur-index (+ beat (* cur-index seq1-len)))
+      (play-seq m (+ beat (* cur-index seq1-len)) seq1))))
+
+;; refactor!
+(defn piso2 [m beat tonic type]
+  (let [seq1 (calc-seq tonic type 17 0 pi1000)
+        seq1-len (num-beats seq1)
+        num-play-rests 4
+        pi-seq (take num-play-rests (drop num-play-rests pi1000))
+        repeat-counts (take-nth 2 pi-seq)
+        rest-count-sums (conj (take-nth 2 (drop 1 (reductions + pi-seq))) 0)
+        seq-indexes (doall (flatten (map #(map (fn [x] (+ %2 x)) %1)
+                                         (map range repeat-counts)
+                                         rest-count-sums)))
+        ]
+    (println "play-seq2 indexes & len" seq-indexes seq1-len)
+    (doseq [cur-index seq-indexes]
+      (println "play-seq2" cur-index (+ beat (* cur-index seq1-len)))
+      (play-seq m (+ beat (* cur-index seq1-len)) seq1))))
+
+
+(defn piso [m beat tonic type]
+  (do
+    ;;(piso0 m beat tonic type)))
+    (piso1 m beat tonic type)
+    (piso2 m (+ beat 17) tonic type)))
+
+;; and now, we play...
+;;(def pfx (inst-fx sampled-piano fx-freeverb))
+;;(ctl pfx :room-size 1.5)
+;;(ctl pfx :dampening 0.5)
+;;(ctl pfx :wet-dry   0.5) ;; dry = direct.  wet = reflections
+(defsynth fx-lpf
+  [bus 0 freq 20000]
+  (let [src (in bus)]
+    (replace-out bus (lpf src freq))))
+(def qfx (inst-fx sampled-piano fx-reverb))
+(def mfx (inst-fx sampled-piano fx-lpf))
+(ctl mfx :freq      2400)
+(def metro (metronome 200))
+(piso metro (metro) :c3 :pentatonic)
+;;(stop)
+;;(clear-fx sampled-piano)
+
+;; should also try 'panning' the cutoff frequencies, etc. during music
