c4d3517612352731b0859e053fb24baf3bc1b960
Roger Allen <rallen@gmail.com>
2012-07-02 07:51:09 -0700

    go to 0.7.1

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject explore_overtone "0.0.0"
   :description "Explorations while trying to learn overtone."
-  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
+  ;;:repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [ [org.clojure/clojure "1.3.0"]
                   [org.clojure/tools.trace "0.7.3"]
-                  ;;[overtone "0.6.0"]
-                  [overtone "0.7.0-LOCALSNAPSHOT"]
+                  [overtone "0.7.1"]
                   [quil "1.5.0"]
                   ]
   :plugins [ [lein-swank "1.4.4"] ]
