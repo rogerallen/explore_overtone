f9c442e75a5fcdedc8de62f745cd80c04d84f965
Roger Allen <rallen@gmail.com>
2012-07-25 23:06:25 -0700

    with the help of Tony Grabowski, changing to use differential equations.  some work to do, but seems promising.

--- src/explore_overtone/my_rhythm.clj
+++ src/explore_overtone/my_rhythm.clj
@@ -2,6 +2,8 @@
   (:use [overtone.music time]
         [overtone.music.rhythm]))
 
+;; DOCS NOT UP-TO-DATE! New method to describe.  See VariableRateMetronome2...
+
 ;; A Variable Rate (vr) metronome is a metronome that takes a tempo
 ;; function and uses integration to find the current beat.
 ;;
@@ -45,16 +47,21 @@
 ;; that are likely a major issue with the current implementation.
 ;;
 
+;; Found an old discussion on this:
+;; http://lalists.stanford.edu/lad/1999/05/0127.html
+;; and I see that there is something similar in the pythonsound project "timewarp"
+;; http://pythonsound.sourceforge.net/guide/x240.html
+
 (defn bpm2mspb
   "beats/min -> milliseconds/beat"
   [bpm]
-  (* 1000 (/ 60.0 (java.lang.Math/abs bpm))))
+  (* 1000 (/ 60.0 (Math/abs bpm))))
 
 ;; (now) ticks are in ms, not s.
 (defn bpm2bps
   "beats/min -> beats/millisecond"
   [bpm]
-  (/ (java.lang.Math/abs bpm) 60.0))
+  (/ (Math/abs bpm) 60.0))
 
 (defn bps2bpm
   "beats/sec -> beats/min"
@@ -78,6 +85,88 @@
               y1 (if (empty? ys1) (last ys0) (first ys1))
               m (/ (- y1 y0) (- x1 x0))]
           (+ y0 (* m (- x x0))))))))
+
+(defn pwl-fn-integrate
+  "y = m x + b.  'y = 1/2 m x^2 + b x + y0"
+  [x0 y0 x1 y1 x]
+  (let [m (/ (- y1 y0) (- x1 x0))
+        dx (- x x0)]
+    (+ 0 (* y0 dx) (* 1/2 m dx dx))))
+
+;; need help to determine y0-y1 range in this case
+(defn lin-time2beat
+  "returns the beat corresponding to current time t (in
+seconds). y0,v0 and y1,v1 are beat,tempo(in beats/second) start & end
+points."
+  [y0 v0 y1 v1 t]
+  (let [a v0
+        b (/ (- v1 v0) (- y1 y0))
+        y (+ y0 (* (/ a b) (- (Math/exp (* b t)) 1.0)))]
+    y))
+
+;; easy to determine y0-y1 range in this case
+(defn lin-beat2time
+  "returns the time (in seconds) corresponding to current beat y.
+y0,v0 and y1,v1 are beat,tempo(in beats/second) start & end
+points."
+  [y0 v0 y1 v1 y]
+  (let [a v0
+        b (/ (- v1 v0) (- y1 y0))
+        u (+ a (* b (- y y0)))
+        t (/ (Math/log (/ u a)) b)]
+    t))
+
+(defn pwl-time2beat
+  "Given a list of [x0 y0 x1 y1 ... xN yN] tempo control points, along
+  with a current x (time) value, solve for the beat value & return it.
+  If x < x0 or x > xN, return nil"
+  [control-points x]
+  ;; correllate control points with time first.  solve for all t
+  ;; values & store in ts first.  FIXME-optimize & do this once.
+  (let [ts (reductions + (conj (map #(apply lin-beat2time %)
+                                    (map vector
+                                         (take-nth 2 control-points)
+                                         (take-nth 2 (drop 1 control-points))
+                                         (take-nth 2 (drop 2 control-points))
+                                         (take-nth 2 (drop 3 control-points))
+                                         (take-nth 2 (drop 2 control-points)))) 0.0))
+        [ts0 ts1] (split-with #(<= % x) ts)
+        [xs0 xs1] (split-at (count ts0) (take-nth 2 control-points))
+        [ys0 ys1] (split-at (count ts0) (take-nth 2 (drop 1 control-points)))
+        ]
+    (if (empty? xs0) nil ;; x < x0
+        (if (empty? xs1) nil ;; x >= xN
+            (let [x0 (last xs0)
+                  x1 (first xs1)
+                  y0 (last ys0)
+                  y1 (first ys1)
+                  t0 (last ts0)]
+              (lin-time2beat x0 y0 x1 y1 (- x t0)))))))
+
+(defn pwl-beat2time
+  "Given a list of [x0 y0 x1 y1 ... xN yN] tempo control points, along
+  with a current x (beat) value, solve for the time value & return it.
+  If x < x0 or x > xN, return nil"
+  [control-points x]
+  (let [[xs0 xs1] (split-with #(<= % x) (take-nth 2 control-points))
+        [ys0 ys1] (split-at (count xs0) (take-nth 2 (drop 1 control-points)))]
+    (if (empty? xs0)
+      nil ;; x < x0
+      (if (empty? xs1)
+        nil ;; x >= xN ;; FIXME -- special case x = xN
+        (let [x0 (last xs0)
+              x1 (first xs1)
+              y0 (last ys0)
+              y1 (first ys1)
+              t0 (reduce + (map #(apply lin-beat2time %)
+                                (map vector
+                                     xs0
+                                     ys0
+                                     (drop 1 xs0)
+                                     (drop 1 ys0)
+                                     (drop 1 xs0))))]
+          (+ t0 (lin-beat2time x0 y0 x1 y1 x)))))))
+
 
 ;; functions from John Lawrence Aspden blog discussion
 (defn booles-rule [f a b]
@@ -106,23 +195,31 @@
 (defn integrate [f a b]
   ;; accurate to 5/100 of a beat or second...good enough?
   ;; something to keep investigating...
-  (adaptive-rule-recurse booles-rule f a b 0.05))
+  (adaptive-rule-recurse booles-rule f a b 5))
 
 ;; Add 2 functions to the Metronome protocol
 (defprotocol VRMetronome
+  (vr-metro-beat [metro ticks]
+    "convert (now) to a precise floating-point beat value.  Not a whole number.")
   (vr-metro-now-beat [metro]
     "convert (now) to a precise floating-point beat value.  Not a whole number.")
   (vr-metro-time [metro b]
     "convert beat b to a precise time value in milliseconds (ticks)."))
 
+;; ======================================================================
+;; Variable Rate Metronome (first...not sure it works)
+;; ======================================================================
 (deftype VariableRateMetronome [start bps-fn mspb-fn]
 
   VRMetronome
   ;; distance (beats) = rate (beats/second) * time (seconds)
   ;; distance (beats) = integrate rate-fn(bps) dt
+  (vr-metro-beat [metro ticks]
+    "convert ticks (in ms) to a precise floating-point beat value.  Not a whole number."
+    (integrate @bps-fn 0 (* 0.001 (- ticks @start)))) ;; time in seconds, not ms
   (vr-metro-now-beat [metro]
     "convert (now) to a precise beat value"
-    (integrate @bps-fn 0 (* 0.001 (- (now) @start)))) ;; time in seconds, not ms
+    (vr-metro-beat metro (now)))
   ;; time (seconds) = (1/rate) (seconds/beat) * distance (beats)
   ;; time (seconds) = integrate rate-fn(spb) dBeats
   (vr-metro-time [metro b]
@@ -185,6 +282,71 @@
     (VariableRateMetronome. start bps-fn mspb-fn)))
 
 ;; ======================================================================
+;; Variable Rate Metronome #2 -- using more direct methods
+;; ======================================================================
+(deftype VariableRateMetronome2 [start beat2bps-fn time2beat-fn beat2time-fn]
+
+  VRMetronome
+  (vr-metro-beat [metro ticks]
+    "convert ticks (in ms) to a precise floating-point beat value.  Not a whole number."
+    (let [delta-ticks (- ticks @start)
+          delta-seconds (* 0.001 delta-ticks)]
+      (@time2beat-fn delta-seconds)))
+  (vr-metro-now-beat [metro]
+    "convert (now) to a precise beat value"
+    (vr-metro-beat metro (now)))
+  (vr-metro-time [metro b]
+    "convert b to a precise time in ticks (ms)"
+    (+ @start (* 1000 (@beat2time-fn b))))
+
+  IMetronome
+  (metro-start [metro] @start)
+  (metro-start [metro start-beat]
+    (let [new-start (- (now) (- (vr-metro-time metro start-beat) @start))]
+      (reset! start new-start)
+      new-start))
+  (metro-tick  [metro] (bpm2mspb (bps2bpm (@beat2bps-fn (vr-metro-now-beat metro)))))
+  (metro-beat  [metro] (inc (long (vr-metro-now-beat metro))))
+  (metro-beat  [metro b] (vr-metro-time metro b))
+  (metro-bpm   [metro] (bps2bpm (@beat2bps-fn (vr-metro-now-beat metro))))
+  (metro-bpm   [metro control-points]
+    (let [cur-beat (metro-beat metro)
+          new-beat2bps-fn (partial pwl-fn control-points)
+          new-time2beat-fn (partial pwl-time2beat control-points)
+          new-beat2time-fn (partial pwl-beat2time control-points)
+          new-start (- (metro-beat metro cur-beat) (* 1000 (beat2time-fn cur-beat)))]
+      (reset! start new-start)
+      (reset! beat2bps-fn new-beat2bps-fn)
+      (reset! time2beat-fn new-time2beat-fn)
+      (reset! beat2time-fn new-beat2time-fn)
+      nil)) ;; return is different from metronome.  okay?
+
+  clojure.lang.ILookup
+  (valAt [this key] (.valAt this key nil))
+  (valAt [this key not-found]
+    (cond (= key :start) @start
+          (= key :bpm) (metro-bpm this)
+          :else not-found))
+
+  clojure.lang.IFn
+  (invoke [this] (metro-beat this))
+  (invoke [this arg]
+    (cond
+     (number? arg) (metro-beat this arg)
+     (= :bpm arg) (metro-beat this)
+     :else (throw (Exception. (str "Unsupported metronome arg: " arg)))))
+  ;; I don't know what this is. ??? (invoke [this _ new-bpm] (.bpm this new-bpm)))
+  )
+
+(defn pwl-metronome ;; FIXME bpm -> beats-per-second 
+  [control-points]
+  (let [start (atom (now))
+        beat2bps-fn (atom (partial pwl-fn control-points))
+        time2beat-fn (atom (partial pwl-time2beat control-points))
+        beat2time-fn (atom (partial pwl-beat2time control-points))]
+    (VariableRateMetronome2. start beat2bps-fn time2beat-fn beat2time-fn)))
+
+;; ======================================================================
 ;; Testing...
 ;; ======================================================================
 #_(
@@ -226,7 +388,7 @@
              #(pwl-fn [0.0 60.0 6.0 240.0 10.0 240.0 12.0 60.0] (mod % 12))))
    ;; this has a nice "swing"...cosine wave with a period of 4 beats.
    (def vrm (vr-metronome
-             #(+ 180 (* 90 (java.lang.Math/cos (/ (* % 2 3.14159265358979) 4.0))))))
+             #(+ 180 (* 90 (Math/cos (/ (* % 2 3.14159265358979) 4.0))))))
 
    ;; this one is really bad since it has a huge discontinuity at 10.0-10.1.
    ;; Just for illustration--don't do this unless you want to hang.
@@ -235,13 +397,13 @@
 
    ;; ----------------------------------------------------------------------
    ;; notes & testing below...uninteresting for others, likely.
-   ;; ----------------------------------------------------------------------
+   ;; 
    ;; trying a varying sine wave + offset works "okay" but the naive
    ;; integrator can get messed up.  We need to work around a
    ;; serious issue -- as time goes on and ((now) - start-beat) gets
    ;; large, the integrator gets more & more inaccurate & the beat can
    ;; go backwards in time.
-   (defn mytempo [x] (+ 120 (* 60 (java.lang.Math/sin (/ x 3.1415)))))
+   (defn mytempo [x] (+ 120 (* 60 (Math/sin (/ x 3.1415)))))
    (def nm (metronome 100.0))
    (def vrm (vr-metronome mytempo))
    (dotimes [i 20]
@@ -276,5 +438,44 @@
    (metro-start nm (nm)) (metro-start vrm (vrm))
    (test-nm-vrm nm vrm)
 
-   
+   ;; ----------------------------------------------------------------------
+   ;; this shows that vr-metro-time & vr-metro-beat don't match up.
+   ;; Current implementation has issues...
+   (def vrm (vr-metronome #(pwl-fn [0.0 200.0 32.0 400.0] %)))
+   (def vrm (vr-metronome #(pwl-fn [0.0 20.0 32.0 40.0] %)))
+   (def vrm (pwl-metronome [0.0 1.0 50.0 2.0 100.0 1.0]))
+   (defn test-b-t [vrm]
+     ;;(println "b" "dt" "b'")
+     (println "(def ds (to-dataset [")
+     (dotimes [i 11]
+       (let [s (* i 10.0)
+             startbeat (metro-start vrm)
+             st (long (vr-metro-time vrm s))
+             dst (* 0.001 (- st startbeat))
+             sb (vr-metro-beat vrm st)]
+         ;;(println (format "%.2f %.3f %.3f %.3f %s"
+         ;;                 s dst sb (Math/abs (- s sb)) (< (Math/abs (- s sb)) 0.01)))
+         (println (format "{\"beat\" %.2f \"time\" %.3f \"beat2\" %.3f \"delta\" %.3f}"
+                          s dst sb (Math/abs (- s sb))))
+         ))
+     (println "]))")
+     )
+   (test-b-t vrm)
+
+   (with-data ds
+     (doto (xy-plot :time :beat)
+       (add-lines :time :beat2)
+       (view)))
+
+   ;; use 0 60bpm 100 120bpm => 0 1bps 100 2bps
+   (def vrm (vr-metronome #(pwl-fn [0.0 60.0 100.0 120.0] %)))
+   ;; bps -> beats
+   (view (function-plot (fn [x] (+ (/ (* x x) -400) x)) 0 100))
+   (with-data ds
+     (doto (xy-plot :beat :time)
+       (add-function (fn [x] (+ (/ (* x x) -400) x)) 0 100)
+       (add-lines :beat2 :time)
+       (view)))
+
+
 )
