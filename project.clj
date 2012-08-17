f12f44b479b1e606766d36da92fb7eb675b9b4bb
Roger Allen <rallen@gmail.com>
2012-08-17 07:00:05 -0700

    update for issue 4 to run local build

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject explore_overtone "0.0.0"
   :description "Explorations while trying to learn overtone."
-  ;;:repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [ [org.clojure/clojure "1.3.0"]
                   [org.clojure/tools.trace "0.7.3"]
-                  [overtone "0.7.1"]
+                  ;;[overtone "0.7.1"]
+                  [overtone "0.8.0-SNAPSHOT"]
                   [quil "1.5.0"]
                   ]
   :plugins [ [lein-swank "1.4.4"] ]
