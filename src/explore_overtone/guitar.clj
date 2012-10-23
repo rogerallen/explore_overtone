d47e6cf7a7749c5e7543a07e106cb9f60a8ba151
Roger Allen <rallen@gmail.com>
2012-10-22 23:08:38 -0700

    Rearranged the guitar code to be more concise & far less embarassing.  Fixed examples to match.

--- src/explore_overtone/guitar.clj
+++ src/explore_overtone/guitar.clj
@@ -1,11 +1,13 @@
-(ns explore_overtone.guitar)
-;; A Simple Guitar by Roger Allen.
-;; ----------------------------------------------------------------------
-(use 'overtone.live)
+;; A Guitar by Roger Allen.
+(ns explore_overtone.guitar
+  (:use [overtone.music pitch time]
+        [overtone.studio inst]
+        [overtone.sc envelope node server ugens]
+        [overtone.sc.cgens mix]))
 
+;; a map of chords to frets.
+;; -1 indicates you don't play that string
 (def guitar-chord-frets
-  ;; a map of chords to frets.
-  ;; -1 indicates you don't play that string
   {:C   [ -1  3  2  0  1  0 ]
    :F   [ -1 -1  3  2  1  1 ]
    :G   [  3  2  0  0  0  3 ]
@@ -45,132 +47,119 @@
    :Gb7 [ -1 -1  4  3  2  0 ]
    :B7  [ -1  2  1  2  0  2 ]
 
-   :C9  [  3, -1,  2,  3,  3,  3 ]
-   :F9  [ -1,  3, -1,  2,  4,  3 ]
-   :G9  [ -1, -1,  0,  2,  0,  1 ]
-   :D9  [ -1, -1,  4,  2,  1,  0 ]
-   :A9  [  0,  0,  2,  4,  2,  3 ]
-   :E9  [  0,  2,  0,  1,  3,  2 ]
-   :Bb9 [ -1, -1,  0,  1,  1,  1 ]
-   :Eb9 [ -1, -1,  1,  0,  2,  1 ]
-   :Ab9 [ -1, -1,  1,  3,  1,  2 ]
-   :Db9 [  4, -1,  3,  4,  4,  4 ]
-   :Gb9 [ -1,  4, -1,  3,  5,  4 ]
-   :B9  [  2, -1,  1,  2,  2,  2 ]
+   :C9  [  3 -1  2  3  3  3 ]
+   :F9  [ -1  3 -1  2  4  3 ]
+   :G9  [ -1 -1  0  2  0  1 ]
+   :D9  [ -1 -1  4  2  1  0 ]
+   :A9  [  0  0  2  4  2  3 ]
+   :E9  [  0  2  0  1  3  2 ]
+   :Bb9 [ -1 -1  0  1  1  1 ]
+   :Eb9 [ -1 -1  1  0  2  1 ]
+   :Ab9 [ -1 -1  1  3  1  2 ]
+   :Db9 [  4 -1  3  4  4  4 ]
+   :Gb9 [ -1  4 -1  3  5  4 ]
+   :B9  [  2 -1  1  2  2  2 ]
 
    :Gadd5 [  3  2  0  0  3  3 ]
    :Cadd9 [ -1  3  2  0  3  3 ]
    :Dsus4 [ -1 -1  0  2  3  3 ]
    })
 
-;; the guitar string instrument is not freed.  Needs to be silenced with
-;; a gate ->0 transition before a gate -> 1 transition activates it.
-;; testing showed it needed something > 25 ms between these transitions.
-;; output is sent on a bus, so that it can be mixed together with
-;; the other strings.
-(defsynth guitar-string-synth
-  [out-bus   {:default 16 :rate :ar}
-   note      {:default 60 :min 0 :max 127}
-   dur       {:default 10.0}
-   decay     {:default 30} ;; pluck decay
-   coef      {:default 0.3 :min -1 :max 1} ;; pluck coef
-   noise-amp {:default 0.8 :min 0.0 :max 1.0}
-   gate      {:default 1}]
-  (let [frq  (midicps note)
-        nze  (* noise-amp (pink-noise))
-        dly  (/ 1.0 frq)
-        plk  (pluck nze gate (/ 1.0 8.0) dly decay coef)
-        snd  (leak-dc (* plk (env-gen (asr 0.0001 1 0.1) :gate gate )) 0.995)]
-    (out out-bus snd)))
-
-;; the "amp" in the guitar that mixes the strings together
-;; and adds optional distortion.
-(defsynth guitar-synth [in-bus-0 {:default 16 :rate :ar}
-                        in-bus-1 {:default 16 :rate :ar}
-                        in-bus-2 {:default 16 :rate :ar}
-                        in-bus-3 {:default 16 :rate :ar}
-                        in-bus-4 {:default 16 :rate :ar}
-                        in-bus-5 {:default 16 :rate :ar}
-                        pre-amp  {:default 1.0}
-                        amp      {:default 1.0}
-                        distort  {:default 0.0 :min 0.0 :max 0.9999999999}]
-  (let [src (* pre-amp (mix [(in in-bus-0 1) (in in-bus-1 1) (in in-bus-2 1)
-                             (in in-bus-3 1) (in in-bus-4 1) (in in-bus-5 1)]))
-        ;; distortion from fx-distortion2 
-        k (/ (* 2 distort) (- 1 distort))
-        dis (/ (* src (+ 1 k)) (+ 1 (* k (abs src))))]
-    (out 0 (pan2 (* amp dis) 0))))
-
-;; create the individual string busses & synths -- defonce?
-(def guitar-string-bus-0 (audio-bus))
-(def guitar-string-bus-1 (audio-bus))
-(def guitar-string-bus-2 (audio-bus))
-(def guitar-string-bus-3 (audio-bus))
-(def guitar-string-bus-4 (audio-bus))
-(def guitar-string-bus-5 (audio-bus))
-(def guitar-string-0 (guitar-string-synth :position :head guitar-string-bus-0 :gate 0))
-(def guitar-string-1 (guitar-string-synth :position :head guitar-string-bus-1 :gate 0))
-(def guitar-string-2 (guitar-string-synth :position :head guitar-string-bus-2 :gate 0))
-(def guitar-string-3 (guitar-string-synth :position :head guitar-string-bus-3 :gate 0))
-(def guitar-string-4 (guitar-string-synth :position :head guitar-string-bus-4 :gate 0))
-(def guitar-string-5 (guitar-string-synth :position :head guitar-string-bus-5 :gate 0))
-(def guitar-string-synths [guitar-string-0 guitar-string-1 guitar-string-2
-                           guitar-string-3 guitar-string-4 guitar-string-5])
+;; ======================================================================
 ;; an array of 6 guitar strings: EADGBE
 (def guitar-string-notes [(note :e2) (note :a2) (note :d3)
                           (note :g3) (note :b3) (note :e4)])
 
-;; connect up the the full instrument
-(def guitar (guitar-synth :position :tail
-                          guitar-string-bus-0 guitar-string-bus-1 guitar-string-bus-2
-                          guitar-string-bus-3 guitar-string-bus-4 guitar-string-bus-5))
-
-
-;; Given a fret-offset, add to the base note index with special handling for -1
 (defn- fret-to-note
+  "given a fret-offset, add to the base note index with special
+  handling for -1"
   [base-note offset]
   (if (>= offset 0)
     (+ base-note offset)
     offset))
 
-(defn pluck-string-at
-  [string-index fret amp t]
-  (let [the-note (fret-to-note (nth guitar-string-notes string-index) fret)] 
-    ;; turn off the previous note
-    (at t (ctl (guitar-string-synths string-index) :gate 0))
-    ;; if part of the chord, turn on the current note
-    ;; NOTE: there needs to be some time between these 
-    (if (> the-note 0)
-      (at (+ t 50) (ctl (guitar-string-synths string-index) :note the-note :amp amp :gate 1)))))
+(defn- mkarg
+  "useful for making arguments for the guitar strings"
+  [s i]
+  (keyword (format "%s-%d" s i)))
 
-(defn pluck-string
-  [string-index fret amp]
-  (pluck-string-at string-index fret amp (now)))
+;; ======================================================================
+;; Main helper functions.  Use pick or strum to play the guitar instrument.
+(defn pick
+  "pick the guitar instrument's string depending on the fret
+   selected.  A fret value less than -1 will cause no event; -1 or
+   greater causes the previous note to be silenced; 0 or greater will
+   also cause a new note event."
+  ([the-guitar string-index fret t]
+     (let [the-note (fret-to-note (nth guitar-string-notes string-index) fret)] 
+       ;; turn off the previous note
+       (if (>= the-note -1)
+         (at t (ctl the-guitar (mkarg "gate" string-index) 0)))
+       ;; NOTE: there needs to be some time between these
+       ;; FIXME: +50 seems conservative.  Find minimum.
+       (if (>= the-note 0)
+         (at (+ t 50) (ctl the-guitar
+                           (mkarg "note" string-index) the-note
+                           (mkarg "gate" string-index) 1)))))
+  ([the-guitar string-index fret]
+     (pick the-guitar string-index fret (now))))
 
-;; strum a chord on the guitar
-(defn strum-at
-  ([the-time the-chord direction strum-time amp]
-     ;; FIXME - deal with -1 fret offsets--shouldn't count as part of the delta time.
+;; ======================================================================
+(defn strum
+  "strum a chord on the guitar instrument in a direction (:up
+   or :down) with a strum duration of strum-time at t."
+  ([the-guitar the-chord direction strum-time t]
+     ;; FIXME - deal with -1 fret offsets--shouldn't count as part of
+     ;; the delta time.
      (let [dt (* 1000 (/ strum-time 5))
            chord-frets (if (vector? the-chord)
                          the-chord ; treat the chord as a series of frets
                          (guitar-chord-frets the-chord))]
        (dotimes [i 6]
          (let [j (if (= direction :up) (- 5 i) i)]
-           (pluck-string-at j (chord-frets j) amp (+ the-time (* i dt)))))))
-  ([the-time the-chord direction strum-time]
-     (strum-at the-time the-chord direction strum-time 0.5))
-  ([the-time the-chord direction]
-     (strum-at the-time the-chord direction 0.05 0.5))
-  ([the-time the-chord]
-     (strum-at the-time the-chord :down 0.05 0.5)))
+           (pick the-guitar j (chord-frets j) (+ t (* i dt)))))))
+  ([the-guitar the-chord direction strum-time]
+     (strum the-guitar the-chord direction strum-time (now)))
+  ([the-guitar the-chord direction]
+     (strum the-guitar the-chord direction 0.05 (now)))
+  ([the-guitar the-chord]
+     (strum the-guitar the-chord :down 0.05 (now))))
 
-(defn strum-now
-  ([the-chord direction strum-time amp]
-     (strum-at (now) the-chord direction strum-time amp))
-  ([the-chord direction strum-time]
-     (strum-now the-chord direction strum-time 0.5))
-  ([the-chord direction]
-     (strum-now the-chord direction 0.05 0.5))
-  ([the-chord]
-     (strum-now the-chord :down 0.05 0.5)))
+;; ======================================================================
+;; The guitar instrument.
+;; Note: the strings need to be silenced with a gate -> 0 transition
+;; before a gate -> 1 transition activates it.  Testing showed it
+;; needed > 25 ms between these transitions to be effective.
+;; Use the strum and pick helpers to play the instrument.
+(definst guitar [note-0 {:default 60 :min 0 :max 127} gate-0 {:default 0}
+                 note-1 {:default 60 :min 0 :max 127} gate-1 {:default 0}
+                 note-2 {:default 60 :min 0 :max 127} gate-2 {:default 0}
+                 note-3 {:default 60 :min 0 :max 127} gate-3 {:default 0}
+                 note-4 {:default 60 :min 0 :max 127} gate-4 {:default 0}
+                 note-5 {:default 60 :min 0 :max 127} gate-5 {:default 0}
+                 dur       {:default 10.0}
+                 decay     {:default 30} ;; pluck decay
+                 coef      {:default 0.3 :min -1 :max 1} ;; pluck coef
+                 noise-amp {:default 0.8 :min 0.0 :max 1.0}
+                 pre-amp   {:default 1.0}
+                 amp       {:default 1.0}
+                 distort   {:default 0.0 :min 0.0 :max 0.9999999999}]
+  (let [strings (map #(let [frq  (midicps (first %))
+                            nze  (* noise-amp (pink-noise))
+                            plk  (pluck nze
+                                        (second %)
+                                        (/ 1.0 8.0)
+                                        (/ 1.0 frq)
+                                        decay
+                                        coef)]
+                        (leak-dc (* plk (env-gen (asr 0.0001 1 0.1)
+                                                 :gate (second %)))
+                                 0.995))
+                     [[note-0 gate-0] [note-1 gate-1] [note-2 gate-2]
+                      [note-3 gate-3] [note-4 gate-4] [note-5 gate-5]])
+        src (* pre-amp (mix strings))
+        ;; distortion from fx-distortion2 
+        k   (/ (* 2 distort) (- 1 distort))
+        dis (/ (* src (+ 1 k)) (+ 1 (* k (abs src))))]
+    (* amp dis)))
+
--- src/explore_overtone/guitar_examples.clj
+++ src/explore_overtone/guitar_examples.clj
@@ -4,27 +4,30 @@
 
 ;; ======================================================================
 ;; try out the guitar...
-(ctl guitar :pre-amp 4.0) ;; higher values with no distortion, lower with dist
-(ctl guitar :amp 1.0)
-(ctl guitar :distort 0.0) ;; try without distortion, then add it in later
-;; try :distort 0.95, :pre-amp 1.0
-;; ??? what destructive interference is happening?
-;; ??? why does it get "quiet" with distortion?
-(strum-now :E :down 0.5)
-(strum-now :E :up 0.75)
-(strum-now :B :down 0.25)
-(strum-now :A :up 0.5)
+(def g (guitar))
+(ctl g :pre-amp 10.0 :amp 1.0 :distort 0.35) 
+(strum g :E :down 0.25)
+(strum g :E :up 0.75)
+(strum g :B :down 0.25)
+(strum g :A :up 0.5)
+;; bow down to the power chord!
+(ctl g :pre-amp 4.0 :distort 0.99) 
+(strum g [0 2 2 -1 -1 -1])
+(strum g [2 4 4 -1 -1 -1])
 ;; mute all strings
-(strum-now [-1 -1 -1 -1 -1 -1] :down 0.001)
+(strum g [-1 -1 -1 -1 -1 -1])
+(ctl g :pre-amp 10.0 :amp 1.0 :distort 0.15)
 
-;; a little strumming pattern fun
+;; ======================================================================
+;; try out a bit of rhythmic accompanyment
 ;; http://www.youtube.com/watch?v=DV1ANPOYuH8
 ;; http://www.guitar.gg/strumming.html
+;; FIXME -- these strumming patterns need a better treatment
 (defn pat0 [metro cur-measure chord pattern]
   (let [cur-beat (* 4 cur-measure)]
     (doall
      (doseq [[b d] pattern]
-       (strum-at (metro (+ b cur-beat)) chord d)))))
+       (strum g chord d 0.05 (metro (+ b cur-beat)))))))
 (defn dduud [metro cur-measure chord]
   (pat0 metro cur-measure chord
         [ [0.0 :down] [1.0 :down]
@@ -45,6 +48,7 @@
         [ [0.0 :down] [1.0 :down]
           [2.0 :down] [2.25 :up] [2.5 :down] [2.75 :up]
           [3.25 :up] [3.5 :down]]))
+;; now we play...
 (do ;; strumming practice
   (let [metro (metronome 100)
         now (metro)]
@@ -70,40 +74,11 @@
      (doseq [[i c] (map-indexed vector [:Gadd5 :Cadd9 :Gadd5 :Cadd9])]
        (ddudu metro i c))))
   )
-(do ;; evin moar strumming practice
+(do ;; evan moar strumming practice
   (let [metro (metronome 90)
         now (metro)]
     (doall
      (doseq [[i c] (map-indexed vector [:Gadd5 :Cadd9 :Gadd5 :Cadd9])]
        (ddduduud metro i c))))
   )
-
-
-
-;; ======================================================================
-(defsynth string-test-synth
-  [note {:default 60}
-   gate {:default 1}]
-  (let [frq (midicps note)
-        dur 10.0
-        decay 30
-        coef 0.3
-        nze  (* 0.8 (pink-noise))
-        dly  (/ 1.0 frq)
-        plk  (pluck nze gate (/ 1.0 8.0) dly decay coef)
-        snd   (* plk (env-gen (asr 0.0001 1 0.1) :gate gate ))]
-    (out 0 (pan2 snd 0))))
-(def t0 (string-test-synth))
-(do
-  (let [_ (recording-start "~/test.wav")
-        _ (Thread/sleep 500)
-        n (now)]
-    (at (+ n   10) (ctl t0 :note 60 :gate 1))
-    (at (+ n  500) (ctl t0 :note 60 :gate 0))
-    (at (+ n 1000) (ctl t0 :note 60 :gate 1))
-    (at (+ n 1500) (ctl t0 :note 60 :gate 0))
-    (at (+ n 1550) (ctl t0 :note 61 :gate 1))
-    (at (+ n 1900) (ctl t0 :note 61 :gate 0)))
-  (Thread/sleep 4000)
-  (recording-stop))
   
