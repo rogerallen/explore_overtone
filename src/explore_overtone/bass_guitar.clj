09f252f9f6f7433425336bc39005823791c00e20
Roger Allen <rallen@gmail.com>
2012-10-28 21:03:54 -0700

    Add mandolin & bass drum to start.  Some other minor guitar/uke changes.

---
+++ src/explore_overtone/bass_guitar.clj
@@ -0,0 +1,105 @@
+;; A Guitar by Roger Allen.
+(ns explore_overtone.bass-guitar
+  (:use [explore_overtone.stringed]
+        [overtone.music pitch]      
+        [overtone.studio inst]
+        [overtone.sc envelope node server ugens]
+        [overtone.sc.cgens mix]))
+
+;; a map of chords to frets.  This is not all chords, just some of
+;; them as there are many alternatives to choose from.
+;; You can pass in your own arrays to strum, too.
+;; -1 indicates you mute that string
+;; -2 indicates you leave that string alone
+(def bass-guitar-chord-frets
+  ;; FIXME ... these are just the guitar versions
+  {:A    [ -1  0  2  2 ]
+   :A7   [ -1  0  2  0 ]
+   :A9   [  0  0  2  4 ]
+   :Am   [  0  0  2  2 ]
+   :Am7  [  0  0  2  0 ]
+
+   :Bb   [ -1  1  3  3 ]
+   :Bb7  [ -1 -1  3  3 ]
+   :Bb9  [ -1 -1  0  1 ]
+   :Bbm  [ -1 -1  3  3 ]
+   :Bbm7 [  1  1  3  1 ]
+
+   :B    [ -1 -1  4  4 ]
+   :B7   [ -1  2  1  2 ]
+   :B9   [  2 -1  1  2 ]
+   :Bm   [ -1 -1  4  4 ]
+   :Bm7  [ -1  2  0  2 ]
+
+   :C    [ -1  3  2  0 ]
+   :C7   [ -1  3  2  3 ]
+   :C9   [  3  3  2  3 ]
+   :Cm   [  3  3  5  5 ]
+   :Cm7  [  3  3  5  3 ]
+
+   :Db   [ -1 -1  3  1 ]
+   :Db7  [ -1 -1  3  4 ]
+   :Db9  [  4 -1  3  4 ]
+   :Dbm  [ -1 -1  2  1 ]
+   :Dbm7 [ -1  3  2  1 ]
+
+   :D    [ -1 -1  0  2 ]
+   :D7   [ -1 -1  0  2 ]
+   :D9   [ -1 -1  4  2 ]
+   :Dm   [ -1  0  0  2 ]
+   :Dm7  [ -1 -1  0  2 ]
+
+   :Eb   [ -1 -1  5  3 ]
+   :Eb7  [ -1 -1  1  3 ]
+   :Eb9  [ -1 -1  1  0 ]
+   :Ebm  [ -1 -1  4  3 ]
+   :Ebm7 [ -1 -1  1  3 ]
+
+   :E    [  0  2  2  1 ]
+   :E7   [  0  2  0  1 ]
+   :E9   [  0  2  0  1 ]
+   :Em   [  0  2  2  0 ]
+   :Em7  [  0  2  2  0 ]
+
+   :F    [  1  3  3  2 ]
+   :F7   [  1 -1  2  2 ]
+   :F9   [  1  0  3  0 ]
+   :Fm   [  1  3  3  1 ]
+   :Fm7  [  1  3  3  1 ]
+
+   :Gb   [  2  4  4  3 ]
+   :Gb7  [ -1 -1  4  3 ]
+   :Gb9  [ -1  4 -1  3 ]
+   :Gbm  [  2  4  4  2 ]
+   :Gbm7 [  2 -1  2  2 ]
+
+   :G    [  3  2  0  0 ]
+   :G7   [  3  2  0  0 ]
+   :G9   [ -1 -1  0  2 ]
+   :Gm   [ -1 -1  5  3 ]
+   :Gm7  [ -1  1  3  0 ]
+
+   :Ab   [ -1 -1  6  5 ]
+   :Ab7  [ -1 -1  1  1 ]
+   :Ab9  [ -1 -1  1  3 ]
+   :Abm  [ -1 -1  6  4 ]
+   :Abm7 [ -1 -1  4  4 ]
+
+   :Gadd5 [  3  2  0  0 ]
+   :Cadd9 [ -1  3  2  0 ]
+   :Dsus4 [ -1 -1  0  2 ]
+
+   })
+
+;; ======================================================================
+;; an array of 6 bass-guitar strings: EADGBE
+(def bass-guitar-string-notes (map note [:e1 :a1 :d2 :g2]))
+
+;; ======================================================================
+;; Main helper functions.  Use pick or strum to play the ukelele instrument.
+(def pick (partial pick-string bass-guitar-string-notes))
+(def strum (partial strum-strings bass-guitar-chord-frets bass-guitar-string-notes))
+
+;; ======================================================================
+;; Create the bass-guitar definst.  Now via the power of macros
+(gen-stringed-inst bass-guitar 4)
--- src/explore_overtone/mandolin.clj
+++ src/explore_overtone/mandolin.clj
@@ -1,5 +1,5 @@
-;; A Ukelele by Roger Allen.
-(ns explore_overtone.ukelele
+;; A Mandolin by Roger Allen.
+(ns explore_overtone.mandolin
   (:use [explore_overtone.stringed]
         [overtone.music pitch]
         [overtone.studio inst]
@@ -9,78 +9,79 @@
 ;; a map of chords to frets.
 ;; -1 indicates you mute that string
 ;; -2 indicates you leave that string alone
-(def ukelele-chord-frets
-  {:A    [ 2  1  0  0 ]
-   :A7   [ 0  1  0  0 ]
-   :Am   [ 2  0  0  0 ]
-   :Am7  [ 0  0  0  0 ]
+(def mandolin-chord-frets
+  {:A    [ 6  1  0  0 ]
+   :A7   [ 2  2  4  3 ]
+   :Am   [ 5  1  0  0 ]
+   ;;:Am7
 
-   :Bb   [ 3  2  1  1 ]
-   :Bb7  [ 1  2  1  1 ]
-   :Bbm  [ 3  1  1  1 ]
-   :Bbm7 [ 1  1  1  1 ]
+   :Bb   [ 3  0  1  1 ]
+   :Bb7  [ 3  3  5  4 ]
+   :Bbm  [ 3  3  4  6 ]
+   ;;:Bbm7
 
+   ;; FIXME ... finish rest
    :B    [ 4  3  2  2 ]
    :B7   [ 2  3  2  2 ]
    :Bm   [ 4  2  2  2 ]
-   :Bm7  [ 2  2  2  2 ]
+   ;;:Bm7
 
    :C    [ 0  0  0  3 ]
    :C7   [ 0  0  0  1 ]
    :Cm   [ 0  3  3  3 ]
-   :Cm7  [ 3  3  3  3 ]
+   ;;:Cm7 
 
    :Db   [ 1  1  1  4 ]
    :Db7  [ 1  1  1  2 ]
    :Dbm  [ 1  4  4  4 ]
-   :Dbm7 [ 4  4  4  4 ]
+   ;;:Dbm7
 
    :D    [ 2  2  2  0 ]
    :D7   [ 2  2  2  3 ]
    :Dm   [ 2  2  1  0 ]
-   :Dm7  [ 2  2  1  3 ]
+   ;;:Dm7
 
    :Eb   [ 3  3  3  6 ]
    :Eb7  [ 3  3  3  4 ]
    :Ebm  [ 3  6  6  6 ]
-   :Ebm7 [ 3  3  2  4 ]
+   ;;:Ebm7
 
    :E    [ 4  4  4  7 ]
    :E7   [ 1  2  0  2 ]
    :Em   [ 0  4  3  2 ]
-   :Em7  [ 0  2  0  2 ]
+   ;;:Em7
 
    :F    [ 2  0  1  0 ]
    :F7   [ 2  3  1  0 ]
    :Fm   [ 1  0  1  3 ]
-   :Fm7  [ 2  2  1  3 ]
+   ;;:Fm7
 
    :Gb   [ 3  1  2  1 ]
    :Gb7  [ 3  4  2  4 ]
    :Gbm  [ 2  1  2  0 ]
-   :Gbm7 [ 2  4  2  4 ]
+   ;;:Gbm7
 
    :G    [ 0  2  3  2 ]
    :G7   [ 0  2  1  2 ]
    :Gm   [ 0  2  3  1 ]
-   :Gm7  [ 0  2  1  1 ]
+   ;;:Gm7
 
    :Ab   [ 5  3  4  3 ]
    :Ab7  [ 1  3  2  3 ]
    :Abm  [ 4  3  4  2 ]
-   :Abm7 [ 1  3  2  2 ]
+   ;;:Abm7
 
    })
 
 ;; ======================================================================
-;; an array of 4 ukelele strings: GCEA (order low-to-hi is CEGA)
-(def ukelele-string-notes (map note [:g4 :c4 :e4 :a4]))
+;; an array of 4 mandolin strings: GDAE 
+(def mandolin-string-notes (map note [:g3 :d4 :a4 :e5]))
 
 ;; ======================================================================
-;; Main helper functions.  Use pick or strum to play the ukelele instrument.
-(def pick (partial pick-string ukelele-string-notes))
-(def strum (partial strum-strings ukelele-chord-frets ukelele-string-notes))
+;; Main helper functions.  Use pick or strum to play the mandolin instrument.
+(def pick (partial pick-string mandolin-string-notes))
+(def strum (partial strum-strings mandolin-chord-frets mandolin-string-notes))
 
 ;; ======================================================================
-;; Create the ukelele definst.  Now via the power of macros
-(gen-stringed-inst ukelele 4)
+;; Create the mandolin definst.  Now via the power of macros
+(gen-stringed-inst mandolin 4)
