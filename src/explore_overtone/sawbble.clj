0bf602c678b485ca86cd578f90bdbf1cba038342
Roger Allen <rallen@gmail.com>
2012-12-10 21:58:16 -0800

    Update sawbble with final synth.

--- src/explore_overtone/sawbble.clj
+++ src/explore_overtone/sawbble.clj
@@ -3,10 +3,12 @@
         [overtone.gui.control]
         [explore-overtone.scope]))
 
-;; Rewriting this to go step-by-step into creating a saw synth
-(scope :width 700 :height 500) ; watch what we synthesize
+;; Work step-by-step to create a saw synth
 
 ;; ======================================================================
+;; watch what we synthesize
+(scope :width 700 :height 500) 
+
 ;; a helpful controller for playing with the synths
 (defn live-controller [the-synth]
   (let [the-instance (the-synth)
@@ -19,8 +21,7 @@
                       (:params the-synth))
         the-controller (synth-controller the-synth)]
     ;; pass them out of the fn to ensure they stay active
-    (vector the-instance the-watchers the-controller)
-    ))
+    (vector the-instance the-watchers the-controller)))
 
 ;; ======================================================================
 ;; Step 1 - the basic synth 
@@ -114,7 +115,7 @@
 ;;
 (defsynth saw-synth-4
   "a detuned and stereo-separated saw synth with an envelope"  
-  [separation-delay-ms {:default 100 :min 0    :max 500  :step 1}
+  [separation-delay-ms {:default 5.0 :min 0    :max 30.0 :step 0.1}
    right-phase         {:default 1   :min -1   :max 1    :step 2}
    lfo-level           {:default 1.4 :min 0.0  :max 5.0  :step 0.05}
    lfo-freq            {:default 1.8 :min 0.0  :max 10.0 :step 0.1}
@@ -154,7 +155,7 @@
   "a detuned and stereo-separated saw synth with a low-pass-filter."  
   [lpf-freq            {:default 3000 :min 100  :max 10000 :step 100}
    lpf-res             {:default 0.1  :min 0.0  :max 1.0   :step 0.05}
-   separation-delay-ms {:default 100  :min 0    :max 500   :step 1}
+   separation-delay-ms {:default 5.0  :min 0    :max 30.0  :step 0.1}
    right-phase         {:default 1    :min -1   :max 1     :step 2}
    lfo-level           {:default 1.4  :min 0.0  :max 5.0   :step 0.05}
    lfo-freq            {:default 1.8  :min 0.0  :max 10.0  :step 0.1}
@@ -191,25 +192,26 @@
 (defsynth saw-synth-6
   "a detuned and stereo-separated saw synth with a low-pass-filter and
    low-pass-filter LFO."
-  [lpf-lfo-freq       {:default 4.1  :min 0.0 :max 10.0  :step 0.01}
-   lpf-min-freq       {:default 400  :min 100 :max 9900  :step 100}
-   lpf-max-freq       {:default 4000 :min 100 :max 10000 :step 100}
-   lpf-res            {:default 0.1  :min 0.0 :max 1.0   :step 0.05}
-   separation-delay   {:default 0.1  :min 0.0 :max 0.5   :step 0.005}
-   lfo-level          {:default 1.4  :min 0.0 :max 5.0   :step 0.05}
-   lfo-freq           {:default 1.8  :min 0.0 :max 10.0  :step 0.1}
-   pitch-index        {:default 0    :min 0   :max 15    :step 1}
-   adsr-attack-time   {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
-   adsr-decay-time    {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
-   adsr-sustain-level {:default 0.5 :min 0.0  :max 1.0   :step 0.01}
-   adsr-release-time  {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
-   adsr-peak-level    {:default 0.9 :min 0.0  :max 1.0   :step 0.01}
-   adsr-curve         {:default -4  :min -5   :max 5     :step 1}
-   gate               {:default 1.0 :min 0.0  :max 1.0   :step 1}]
+  [lpf-lfo-freq        {:default 4.1  :min 0.0 :max 10.0  :step 0.01}
+   lpf-min-freq        {:default 400  :min 100 :max 9900  :step 100}
+   lpf-max-freq        {:default 4000 :min 100 :max 10000 :step 100}
+   lpf-res             {:default 0.1  :min 0.0 :max 1.0   :step 0.05}
+   separation-delay-ms {:default 5.0  :min 0    :max 30.0  :step 0.1}
+   lfo-level           {:default 1.4  :min 0.0 :max 5.0   :step 0.05}
+   lfo-freq            {:default 1.8  :min 0.0 :max 10.0  :step 0.1}
+   pitch-index         {:default 0    :min 0   :max 15    :step 1}
+   adsr-attack-time    {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
+   adsr-decay-time     {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
+   adsr-sustain-level  {:default 0.5 :min 0.0  :max 1.0   :step 0.01}
+   adsr-release-time   {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
+   adsr-peak-level     {:default 0.9 :min 0.0  :max 1.0   :step 0.01}
+   adsr-curve          {:default -4  :min -5   :max 5     :step 1}
+   gate                {:default 1.0 :min 0.0  :max 1.0   :step 1}]
   (let [pitch-midi (index:kr (:id scale-buffer) pitch-index)
         pitch-freq (midicps pitch-midi)
         lfo-out (* lfo-level (sin-osc lfo-freq))
         saws-out (mix (saw [pitch-freq (+ pitch-freq lfo-out)]))
+        separation-delay (/ separation-delay-ms 1000.0)
         saws-out-2ch [saws-out (delay-c saws-out 1.0 separation-delay)]
         lpf-freq (lin-lin (sin-osc lpf-lfo-freq) -1 1 lpf-min-freq lpf-max-freq)
         lpf-out-2ch (moog-ff saws-out-2ch lpf-freq lpf-res)
@@ -223,51 +225,51 @@
 (live-controller saw-synth-6)
 
 ;; ======================================================================
-;; The full synth.  FIXME reconcile the parameters, etc.
+;; The full synth with some added slew on the input notes
 (defsynth sawbble-synth
-  [note          60
-   volume         1.0
-   attack         0.01
-   decay          0.3
-   sustain        0.5
-   release        0.01
-   level          1.0
-   curve         -4
-   gate           1
-   lfo-level      0.5
-   lfo-freq       2.0
-   lp-freq-lo  400.0
-   lp-freq-hi 4000.0
-   lp-lfo-freq   7.0
-   lp-res        0.3
-   note-slew    50.0
-   sep-delay     0.1
-   ]
-  (let [note       (slew:kr note note-slew note-slew)
-        note-freq  (midicps note)
-        saw-out    (saw note-freq)
-        lfo-out    (* lfo-level (sin-osc lfo-freq))
-        saw-out2   (saw (+ note-freq lfo-out))
-        ;; it really is amazing the difference you add two detuned saws...
-        saws       (mix [saw-out saw-out2])
-        lp-lfo-out (lin-lin (sin-osc lp-lfo-freq) -1 1 lp-freq-lo lp-freq-hi)
-        lp-out     (moog-ff saws lp-lfo-out lp-res)
-        lp-out     [(delay-c lp-out 1 0.0) (delay-c lp-out 1 sep-delay)]
-        env-out    (env-gen (adsr attack decay sustain release level curve)
-                            :gate gate :action FREE)
-        ]
-    (out 0 (* volume env-out lp-out))))
+  "a detuned and stereo-separated saw synth with a low-pass-filter and
+   low-pass-filter LFO."
+  [note                {:default 60   :min 0   :max 127   :step 1}
+   note-slew           {:default 15.0 :min 1.0 :max 50.0  :step 1.0}
+   separation-delay-ms {:default 5.0  :min 0   :max 30.0  :step 0.1}
+   lpf-lfo-freq        {:default 4.1  :min 0.0 :max 10.0  :step 0.01}
+   lpf-min-freq        {:default 400  :min 100 :max 9900  :step 100}
+   lpf-max-freq        {:default 4000 :min 100 :max 10000 :step 100}
+   lpf-res             {:default 0.1  :min 0.0 :max 1.0   :step 0.05}
+   lfo-level           {:default 1.4  :min 0.0 :max 5.0   :step 0.05}
+   lfo-freq            {:default 1.8  :min 0.0 :max 10.0  :step 0.1}
+   adsr-attack-time    {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
+   adsr-decay-time     {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
+   adsr-sustain-level  {:default 0.5 :min 0.0  :max 1.0   :step 0.01}
+   adsr-release-time   {:default 0.1 :min 0.0  :max 1.0   :step 0.01}
+   adsr-peak-level     {:default 0.9 :min 0.0  :max 1.0   :step 0.01}
+   adsr-curve          {:default -4  :min -5   :max 5     :step 1}
+   gate                {:default 1.0 :min 0.0  :max 1.0   :step 1}]
+  (let [pitch-midi (slew:kr note note-slew note-slew) ;; interesting lag to changing notes
+        pitch-freq (midicps pitch-midi)
+        lfo-out (* lfo-level (sin-osc lfo-freq))
+        saws-out (mix (saw [pitch-freq (+ pitch-freq lfo-out)]))
+        separation-delay (/ separation-delay-ms 1000.0)
+        saws-out-2ch [saws-out (delay-c saws-out 1.0 separation-delay)]
+        lpf-freq (lin-lin (sin-osc lpf-lfo-freq) -1 1 lpf-min-freq lpf-max-freq)
+        lpf-out-2ch (moog-ff saws-out-2ch lpf-freq lpf-res)
+        env-out (env-gen (adsr adsr-attack-time   adsr-decay-time
+                               adsr-sustain-level adsr-release-time
+                               adsr-peak-level    adsr-curve)
+                         :gate gate :action FREE)] ;; FREE at last!
+    (out 0 (* env-out lpf-out-2ch))))
 
-;; (def sawbble (sawbble-synth 30))
+;; some things to play with...
+;; (def sawbble (sawbble-synth))
 ;; (stop)
-;; (ctl sawbble :note 35)
+;; (ctl sawbble :note 35 :gate 1)
 ;; (ctl sawbble :note 32)
 ;; (ctl sawbble :note 30)
 ;; (ctl sawbble :lfo-level 1.5 :lfo-freq 2.0)
-;; (ctl sawbble :note-slew 15.0)
-;; (ctl sawbble :lp-freq-lo 200.0 :lp-freq-hi 4000.0 :lp-lfo-freq 5.0)
+;; (ctl sawbble :note-slew 25.0)
+;; (ctl sawbble :lpf-freq-lo 200.0 :lpf-freq-hi 4000.0 :lpf-lfo-freq 1.5 :lpf-res 0.9)
 ;; (ctl sawbble :lp-res 0.75)
-;; (ctl sawbble :sep-delay 0.02)
+;; (ctl sawbble :sep-delay 2.0)
 ;; (ctl sawbble :gate 0)
 
 (defn play-notes [the-inst level m notes]
@@ -291,9 +293,9 @@
   (partial sawbble-synth 
            :lfo-level 1.5 :lfo-freq 0.7
            :note-slew 50.0
-           :lp-freq-lo 500.0 :lp-freq-hi 4000.0 :lp-lfo-freq 0.35
-           :lp-res 0.2
-           :sep-delay 0.05
+           :lpf-freq-lo 500.0 :lpf-freq-hi 4000.0 :lpf-lfo-freq 0.5
+           :lpf-res 0.2
+           :sep-delay 2.5
            ))
            
 ;; staccatto
