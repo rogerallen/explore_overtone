75fc373a039d124ae56ba7723f76d466ca13f37a
Roger Allen <rallen@gmail.com>
2013-01-13 16:58:01 -0800

    Go to RC15.  All looks good to me.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Explorations while learning Overtone."
   :dependencies [ [org.clojure/clojure "1.3.0"]
                   [org.clojure/tools.trace "0.7.3"]
-                  [overtone "0.8.0-RC14"]
+                  [overtone "0.8.0-RC15"]
                   [quil "1.6.0"]
                   [oversampler "0.2.0-SNAPSHOT"]
                   [leipzig "0.2.0-RALLEN"]
