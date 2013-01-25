6242d4695932756faec5092a1363a7e723f048d3
Roger Allen <rallen@gmail.com>
2013-01-25 07:33:32 -0800

    Rearrange code to make some logical sense.  Seems nice & clean.

--- src/explore_overtone/midipe.clj
+++ src/explore_overtone/midipe.clj
@@ -1,9 +1,25 @@
+;; ======================================================================
+;; basic midi note-on/off input persistence
+;;
+;; Usage:
+;;   (require '[explore-overtone.midipe :as mp])
+;;   (mp/new)    ;; start recording to a new file
+;;   (mp/pause)  ;; pause recording
+;;   (mp/record) ;; restart recording
+;;   (mp/save)   ;; save data to file
+;;   (mp/open "my-saved-events.clj") ;; read events from saved file
+;; ======================================================================
 (ns explore-overtone.midipe
   (:use [overtone.music.time :only [now]]
         [overtone.libs.event :only [on-event remove-handler]]))
 
-;; basic midi input persistence
+;; ======================================================================
+;; state atoms
+(def midipe-dirty (atom false))
+(def midipe-filename (atom nil))
+(def midipe-events (atom []))
 
+;; ======================================================================
 (defn- to-file
   "Save a clojure form to a file"
   [#^java.io.File file form]
@@ -16,43 +32,29 @@
   (with-open [r (java.io.PushbackReader. (java.io.FileReader. file))]
     (read r)))
 
-(defn get-filename
-  ([x] (get-filename)) ;; ignore the arg
+(defn- get-timestamp-filename
   ([] (str "midipe_"
        (.format (java.text.SimpleDateFormat. "yyMMdd_HHmmss") (now))
-       ".cljp")))
+       ".clj")))
 
-(def midipe-dirty (atom false))
-(defn dirty! []
+(defn- dirty! []
   (swap! midipe-dirty (fn [x] true)))
-(defn clean! []
+
+(defn dirty? []
+  @midipe-dirty)
+
+(defn- clean! []
   (swap! midipe-dirty (fn [x] false)))
 
-(def midipe-filename (atom (get-filename)))
-
-(def midipe-events (atom []))
-
-(defn new-midipe []
-  ;;(save-midipe)
-  (swap! midipe-filename get-filename)
-  (swap! midipe-events (fn [x] []))
-  (clean!))
-
-(defn add-event! [e]
+(defn- add-event! [e]
   (swap! midipe-events conj e)
   (dirty!))
 
-(defn save []
-  (to-file (java.io.File. @midipe-filename) @midipe-events)
-  (clean!))
-
-(defn open [filename]
-  (swap! midipe-filename (fn [x] filename))
-  (swap! midipe-events (fn [x] (from-file (java.io.File. filename))))
-  (clean!))
-
-(defn record-events
-  ([] (record-events [:midi] ::midipe))
+;; ======================================================================
+;; public api fillows
+(defn record
+  "Record note on/off events to the midipe-events list"
+  ([] (record [:midi] ::midipe))
   ([device-key player-key]
      (let [on-event-key  (concat device-key [:note-on])
            off-event-key (concat device-key [:note-off])
@@ -77,21 +79,53 @@
                                 :timestamp timestamp}))
                  off-key))))
 
-(defn stop-events
+(defn pause
+  "Pause recording note on/off events to the midipe-events list"
   []
   (remove-handler [::midipe :midi :note-on])
   (remove-handler [::midipe :midi :note-off]))
 
+(defn new
+  "Start a new midi persistence file, losing all prior events.
+Start monitoring events. Returns new filename."
+  []
+  (let [name (get-timestamp-filename)]
+    (swap! midipe-filename (fn [x] name))
+    (swap! midipe-events (fn [x] []))
+    (clean!)
+    (record)
+    name))
+
+(defn save
+  "Save the current events to a file (if necessary).  Returns true if it saved the file."
+  []
+  (if (and (not (nil? @midipe-filename))
+           (dirty?))
+    (do
+      (to-file (java.io.File. @midipe-filename) @midipe-events)
+      (clean!)
+      true)
+    false))
+
+(defn open
+  "Open an existing file and read in the events"
+  [filename]
+  (swap! midipe-filename (fn [x] filename))
+  (swap! midipe-events (fn [x] (from-file (java.io.File. filename))))
+  (clean!)
+  nil)
+
+(defn events
+  "Return the event list"
+  []
+  @midipe-events)
+
 (comment
-  (new-midipe)
-  ;; play some notes...
-  (count @midipe-events) ;; count them
-  (save) ;; save theml
-  (open "midipe_130124_224254.cljp") ;; restore them
-  ;; convert timestamps to seconds since beginning of sequence
   (def first-ts (:timestamp (first @midipe-events)))
   (map #(/ (- (:timestamp %) first-ts) 1000000.0) @midipe-events)
   ;; look at notes
   (map #(:velocity %) (filter #(= (:command %) :note-on) @midipe-events))
 
+  (dirty?)
+  @midipe-dirty
   )
