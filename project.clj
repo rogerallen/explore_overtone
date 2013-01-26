6d2c60be87160552f359273310673bb69fd87c22
Roger Allen <rallen@gmail.com>
2013-01-26 15:53:58 -0800

    Use 0.8.0 overtone, 0.3.0 oversampler

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject explore_overtone "0.0.0"
   :description "Explorations while learning Overtone."
-  :dependencies [ [org.clojure/clojure "1.4.0"]
-                  [org.clojure/tools.trace "0.7.3"]
-                  [overtone "0.8.0-RC20"]
-                  [quil "1.6.0"]
-                  [oversampler "0.3.0-SNAPSHOT"]
-                  [leipzig "0.2.0"]
-                  ]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.trace "0.7.3"]
+                 [overtone "0.8.0"]
+                 [quil "1.6.0"]
+                 [oversampler "0.3.0"]
+                 [leipzig "0.2.0"]]
   :plugins [ [lein-swank "1.4.4"] ]
   :jvm-opts ["-Dfile.encoding=UTF-8"]
   )
