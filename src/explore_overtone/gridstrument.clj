e2c2abce8bfe1342ec7d33fe68492af4727f7c63
Roger Allen <rallen@gmail.com>
2015-05-18 23:25:20 -0700

    Playing around with the guitar synth.

--- src/explore_overtone/gridstrument.clj
+++ src/explore_overtone/gridstrument.clj
@@ -9,9 +9,95 @@
 (def server (osc-server 8675 "gridstrument"))
 ;; (osc-close server)
 
-(gen-stringed-synth pektara 1 false) ;; persistent ektara
+;; ======================================================================
+;; trying to improve the guitar sound
+(defmacro gen-stringed-synth-2
+  "Macro to generate a stringed defsynth with distortion, reverb and
+   a low-pass filter.  Use the pluck-strings and strum-strings helper
+  functions to play the instrument.
+   Note: the strings need to be silenced with a gate -> 0 transition
+   before a gate -> 1 transition activates it.  Testing
+   showed it needed > 25 ms between these transitions to be effective."
+  [name num-strings free-on-silence]
+  (let [note-ins (if (= num-strings 1)
+                   [(symbol "note")]
+                   (apply vector
+                          (map #(symbol (format "note-%d" %)) (range num-strings))))
+        note-default-ins (apply vector
+                                (flatten (map vector
+                                              note-ins
+                                              (repeat num-strings {:default 60 :min 0 :max 127}))))
+        gate-ins (if (= num-strings 1)
+                   [(symbol "gate")]
+                   (apply vector
+                          (map #(symbol (format "gate-%d" %)) (range num-strings))))
+        gate-default-ins (apply vector (flatten (map vector
+                                                     gate-ins
+                                                     (repeat num-strings {:default 0}))))
+        both-default-ins (into note-default-ins gate-default-ins)
+        note-gate-pairs (apply vector (map vector note-ins gate-ins))
+        env-gen-fn (if free-on-silence
+                     '(fn [x] (overtone.sc.ugens/env-gen
+                              (overtone.sc.envelope/asr 0.0001 1 0.1)
+                              :gate (second x)
+                              :action overtone.sc.ugens/FREE))
+                     '(fn [x] (overtone.sc.ugens/env-gen
+                              (overtone.sc.envelope/asr 0.0001 1 0.1)
+                              :gate (second x))))
+        ]
+    `(defsynth ~name
+       ~(str "a stringed instrument synth with " num-strings
+             " strings mixed and sent thru
+  distortion and reverb effects followed by a low-pass filter.  Use
+  the pluck-strings and strum-strings helper functions to play the
+  instrument. Note: the strings need to be silenced with a gate -> 0
+  transition before a gate -> 1 transition activates it."
+             (if free-on-silence
+               " This instrument
+  is transient.  When a string becomes silent, it will be freed."
+               " This instrument
+  is persistent.  It will not be freed when the strings go silent."))
 
-(def pitch-bend-range 10) ;; keep in sync with GridStrument
+       [~@both-default-ins
+        ~'decay     {:default 30    :min 1   :max 100} ;; pluck decay
+        ~'coef      {:default 0.3   :min -1  :max 1}   ;; pluck coef
+        ~'noise-amp {:default 0.8   :min 0.0 :max 1.0}
+        ~'pre-amp   {:default 6.0   :min 0.0 :max 10.0}
+        ~'amp       {:default 1.0   :min 0.0 :max 10.0}
+        ;; by default, no distortion, no reverb, no low-pass
+        ~'distort   {:default 0.0   :min 0.0 :max 0.9999999999}
+        ~'rvb-mix   {:default 0.0   :min 0.0 :max 1.0}
+        ~'rvb-room  {:default 0.0   :min 0.0 :max 1.0}
+        ~'rvb-damp  {:default 0.0   :min 0.0 :max 1.0}
+        ~'lp-freq   {:default 20000 :min 100 :max 20000}
+        ;;~'lp-rq     {:default 1.0   :min 0.1 :max 10.0}
+        ~'pan       {:default 0.0   :min -1  :max 1}
+        ~'out-bus   {:default 0     :min 0   :max 100}]
+       (let [strings# (map #(let [frq#  (midicps (first %))
+                                  nze#  (~'* ~'noise-amp (white-noise))
+                                  plk#  (pluck nze#
+                                               (second %)
+                                               (/ 1.0 8.0)
+                                               (~'/ 1.0 frq#)
+                                               ~'decay
+                                               ~'coef)]
+                              (leak-dc (~'* plk# (~env-gen-fn %))
+                                       0.995))
+                           ~note-gate-pairs)
+             src# (~'* ~'pre-amp (mix strings#))
+             ;; get rid of the mud at the bottom
+             src# (hpf (hpf src# 60) 30)
+             ;; distortion from fx-distortion2
+             k#   (~'/ (~'* 2 ~'distort) (~'- 1 ~'distort))
+             dis# (~'/ (~'* src# (~'+ 1 k#))
+                       (~'+ 1 (~'* k# (abs src#))))
+             vrb# (free-verb dis# ~'rvb-mix ~'rvb-room ~'rvb-damp)
+             fil# (lpf vrb# ~'lp-freq)] ;; lpf, not rlpf
+         (out ~'out-bus (pan2 (~'* ~'amp fil#) ~'pan))))))
+;;(macroexpand-1 '(gen-stringed-synth ektara 1 true))
+
+;;(gen-stringed-synth pektara 1 false) ;; persistent ektara
+(gen-stringed-synth-2 pektara 1 false)
 
 ;; six fingers should be good enough.
 (def eks [(pektara) (pektara) (pektara) (pektara) (pektara) (pektara)])
@@ -20,10 +106,16 @@
 ;; adjust the eks sound to whatever suits you
 (dorun (map
         #(ctl %
-              :pre-amp 5.0
-              :lp-freq 3000 :lp-rq 0.25
-              :rvb-mix 0.3 :rvb-room 0.7 :rvb-damp 0.4)
+              :noise-amp 0.7 :coef 0.5 ;; play with these
+              :decay 50
+              :lp-freq 5000 ;; and this
+              :pre-amp 6.0 :distort 0.75
+              :rvb-mix 0.4 :rvb-room 0.81 :rvb-damp 0.5)
         eks))
+
+(def pitch-bend-range 10) ;; keep in sync with GridStrument
+;;(recording-start "g6.wav")
+;;(recording-stop)
 
 ;; translate GridStrument into awesomeness!
 (defn grid-note
@@ -40,9 +132,14 @@
 (defn grid-cc
   [channel note value]
   (let [norm-value (/ value 127.0)
-        norm-value (* norm-value 0.9)] ;; don't allow 1.0 distortion
+        norm-value (* norm-value 0.95)] ;; don't allow 1.0 distortion
     ;;(println "cc" channel note value norm-value)
-    (ctl (nth eks channel) :distort norm-value)))
+    ;; can't quite figure out what I want to do here.
+    ;;(ctl (nth eks channel) :distort (+ 0.25 (* 0.5 norm-value)))
+    ;;(ctl (nth eks channel) :lp-freq (+ 3200 (* 4000 norm-value)))
+    ;;(ctl (nth eks channel) :rvb-mix (+ 0.5 (* 0.6 (- norm-value 0.5))))
+    nil
+    ))
 
 (defn grid-pitch-bend
   [channel value]
